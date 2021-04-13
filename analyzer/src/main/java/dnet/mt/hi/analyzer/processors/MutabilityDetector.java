7521fa8f70d68a49891e185823e96daf2f8af5ef
Majid Makki <Majid.Makki@cs.kuleuven.be>
2021-04-13 16:11:04 +0200

    MultiTenantScheduledThreadPoolExecutor fixed.

--- analyzer/src/main/java/dnet/mt/hi/analyzer/processors/MutabilityDetector.java
+++ analyzer/src/main/java/dnet/mt/hi/analyzer/processors/MutabilityDetector.java
@@ -1,15 +1,25 @@
 package dnet.mt.hi.analyzer.processors;
 
 import dnet.mt.hi.analyzer.enums.MutabilityStatus;
+import sun.misc.Unsafe;
 
-import java.util.Arrays;
-import java.util.List;
+import java.lang.invoke.MethodType;
+import java.nio.ByteOrder;
+import java.time.*;
+import java.time.chrono.IsoChronology;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DecimalStyle;
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class MutabilityDetector {
 
     private static List<Class> KNOWN_IMMUTABLE_CLASSES = Arrays.asList(Class.class, String.class, Integer.class, Long.class,
-            Character.class, Short.class, Double.class, Float.class, Byte.class, Character.UnicodeBlock.class,
-            Character.UnicodeScript.class);
+            Character.class, Short.class, Double.class, Float.class, Byte.class, Boolean.class, ByteOrder.class,
+            IsoChronology.class, DateTimeFormatter.class, DecimalStyle.class, Instant.class, LocalDate.class, LocalDateTime.class,
+            OffsetDateTime.class, OffsetTime.class, Period.class, ZoneOffset.class, Locale.class, MethodType.class, Unsafe.class,
+            StackWalker.class, Pattern.class, Runtime.Version.class, Optional.class, OptionalDouble.class, OptionalInt.class,
+            OptionalLong.class, Character.UnicodeBlock.class, Character.UnicodeScript.class);
 
     public MutabilityStatus detect(Class clazz) {
         if (clazz.isPrimitive() || clazz.isEnum()) {
