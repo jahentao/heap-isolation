0b58a83f97c625c7a5192a5168af6d7d1488efde
Majid Makki <Majid.Makki@cs.kuleuven.be>
2021-05-22 16:55:11 +0200

    Skeleton for performance evaluation created.

--- evaluation/performance/plugin/src/main/java/dnet/mt/hi/eval/performance/renaissance/plugin/SecurityManagerEnabler.java
+++ evaluation/performance/plugin/src/main/java/dnet/mt/hi/eval/performance/renaissance/plugin/SecurityManagerEnabler.java
@@ -2,10 +2,27 @@ package dnet.mt.hi.eval.performance.renaissance.plugin;
 
 import org.renaissance.Plugin;
 
+import java.security.*;
+
 public class SecurityManagerEnabler implements Plugin {
 
     public SecurityManagerEnabler() {
-        
+        Policy.setPolicy(new Policy() {
+
+            private PermissionCollection ALL_PERMISSION_COLLECTION = new AllPermission().newPermissionCollection();
+
+            @Override
+            public PermissionCollection getPermissions(CodeSource cs) {
+                return ALL_PERMISSION_COLLECTION;
+            }
+
+            @Override
+            public PermissionCollection getPermissions(ProtectionDomain domain) {
+                return getPermissions(domain.getCodeSource());
+            }
+            
+        });
+        System.setSecurityManager(new SecurityManager());
     }
 
 }
